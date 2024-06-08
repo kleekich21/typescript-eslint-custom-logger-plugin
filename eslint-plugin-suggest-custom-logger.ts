b16670d5898ebe256f373092d14f29a1dfd87abf
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-06-08 11:30:23 +0900

    fix: rule name fixed

--- eslint-plugin-suggest-custom-logger.ts
+++ eslint-plugin-suggest-custom-logger.ts
@@ -1,6 +1,6 @@
 import suggestCustomLoggerRule from "./src/rules/suggest-custom-logger-rule";
 
 const customLoggerPlugin = {
-  rules: { "suggest-custom-logger": suggestCustomLoggerRule },
+  rules: { "suggest-custom-logger-rule": suggestCustomLoggerRule },
 };
 export default customLoggerPlugin;
